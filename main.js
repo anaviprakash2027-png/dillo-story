ebbdc6fbcf5400706ded92f83d227eac76d152ed
maiyawahl2027 <maiyawahl2027@u.northwestern.edu>
2026-03-09 17:24:01 -0500

    scrollytelly commit 2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* */
+/*SCROLLYTELLY: start function when detect user scrolling*/
 
 window.addEventListener("scroll", function () {
     const scrollTop = window.scrollY;
@@ -9,19 +9,16 @@ window.addEventListener("scroll", function () {
     progressBar.style.height = (scrollPercent * maxBarHeight) + "px";
 });
 
-/* */
-
-/* */
+/*SCROLLYTELLY: initiatlize variables*/
 var container = d3.select("#scroll");
 var figure = container.select("figure");
 var article = container.select("article");
 var step = article.selectAll(".step");
-/* */
+/*SCROLLYTELLY: call scrollama function*/
 var scroller = scrollama();
 
-/* */
+/*SCROLLYTELLY: resize media box (right) according to step image*/
 function handleResize() {
-    // Right media box: square-like
     var figureWidth = figure.node().offsetWidth;
     var figureHeight = figureWidth;
     figure.style("height", figureHeight + "px");
@@ -29,74 +26,70 @@ function handleResize() {
     scroller.resize();
 }
 
-/* */
+/*SCROLLYTELLY: create media array to store image links*/
 var media = [
         {
-            /* */
+            /*Step 0 image link */
             type: "img",
             src: "DilloPoll.jpg",
             alt: "Mayfest Productions releases poll hinting at potential music genres for 54th Dillo Day",
         },
         {
-            /* */
+            /*Step 2 image link */
             type: "img",
             src: "TDNWArticle.jpg",
             alt: "The Daily Northwestern article announcing new Dillo Day stag",
         }
         ];
 
-        /* */
-        function handleStepEnter(response) {
-            step.classed("is-active", (d, i) => i === response.index);
+/*SCROLLYTELLY: activate steps in order*/
+function handleStepEnter(response) {
+    step.classed("is-active", (d, i) => i === response.index);
             
-            /* */
-            if (response.index === 0) {
-                const item = media[0];
+    /*activate Step 0*/
+    if (response.index === 0) {
+        const item = media[0];
                 
-                d3.select("#scrolly-media-container").html(`
-                    <img src="${item.src}" class="scrolly-media" alt="${item.alt}">
-                    `);
+        d3.select("#scrolly-media-container").html(`
+            <img src="${item.src}" class="scrolly-media" alt="${item.alt}">
+            `);
                 
-                return;
+        return;
 
-            }
+    }
                 
-                /* */
-            if (response.index === 1) {
-                d3.select("#scrolly-media-container").html(`
-                    <iframe src="https://flo.uri.sh/visualisation/27863444/embed"
-                    class="scrolly-media"
-                    frameborder="0"
-                    scrolling="no"
-                    style="width:100%;height:100%;"
-                    sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation">
-                    </iframe>
-                    `);
+    /*activate Step 1*/
+    if (response.index === 1) {
+        d3.select("#scrolly-media-container").html(`
+            <iframe src="https://flo.uri.sh/visualisation/27863444/embed"
+            class="scrolly-media"
+            frameborder="0"
+            scrolling="no"
+            style="width:100%;height:100%;"
+            sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation">
+            </iframe>
+            `);
                         
-                d3.select("#scrolly-link").attr("href", "").text("");
+        d3.select("#scrolly-link").attr("href", "").text("");
                         
-                return;
-            }
+        return;
+    }
                     
                     
-            /* */
-            if (response.index === 2) {
-                const item = media[1];
+    /*activate Step 2*/
+    if (response.index === 2) {
+        const item = media[1];
                         
-                    d3.select("#scrolly-media-container").html(`
-                        <img src="${item.src}" class="scrolly-media" alt="${item.alt}">
-                        `);
+        d3.select("#scrolly-media-container").html(`
+            <img src="${item.src}" class="scrolly-media" alt="${item.alt}">
+            `);
                             
-                return;
+        return;
                         
-            }
-            }
+    }
+}
 
-
-
-         
-
-/* */
+/*SCROLLYTELLY:activate steps when reach certain scroll position*/
 function init() {
     handleResize();
     scroller
